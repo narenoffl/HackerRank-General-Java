9fef42be1fea111892705ad0b890754d1764ae25
Naren Purushothaman <naren.p.2018.cse@ritchennai.edu.in>
2021-07-11 16:04:11 +0530

    Create XElementInArray.java

---
+++ XElementInArray.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Your Code Here
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int arr[] = new int[n];
+		for(int i=0;i<n;i++)
+		{
+		    arr[i] = sc.nextInt();
+		}
+		
+		int x = sc.nextInt();
+		System.out.println(arr[x-1]);
+
+	}
+}
